a3d41a45684c383be2e028adacafaa066520ebc2
DKFN <valere.tetelin@gmail.com>
2019-11-09 13:35:01 +0100

    A bit more complex backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,34 @@
 extern crate env_logger;
+#[macro_use] extern crate serde_derive;
 use actix_files as fs;
-use actix_web::{App, HttpServer, middleware};
+use actix_web::{App, HttpServer, middleware, web};
+use actix_files::NamedFile;
+use json::JsonValue;
+use serde_json::json;
+use actix_web::web::Json;
+use actix_web::middleware::Logger;
+
+// "Model" Json et Database
+#[derive(Deserialize, Serialize)]
+pub struct ModelDeFou {
+    name: String,
+}
+
+// "Controlleur"
+pub fn greet() -> Result<Json<ModelDeFou>, actix_web::Error> {
+    Ok(Json(ModelDeFou { name: "COUCOU".to_string() }))
+}
+
+// autre controlleur :)
+pub fn index() -> Result<NamedFile, actix_web::Error> {
+    let path = "./public/front/index.html";
+    Ok(NamedFile::open(path)?)
+}
 
 pub fn main() {
+
+    let app_port = std::env::var("APP_PORT").unwrap_or("8080".to_string());
+
     let splash = "
    _____ __  ________________  ____    _______   ______________   __________
   / ___// / / / ____/ ____/ / / / /   / ____/ | / / ____/ ____/  /  _/_  __/
@@ -19,12 +45,11 @@ pub fn main() {
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Compress::default())
-            .service(
-            // This folder will be created on docker build
-            fs::Files::new("/", "./public/front/").index_file("index.html")
-        )
+            .route("/", web::get().to(index))
+            .route("/api/test", web::get().to(greet))
+            .wrap(Logger::default())
     })
-        .bind("0.0.0.0:8080")
+        .bind(format!("0.0.0.0:{}", app_port))
         .unwrap()
         .run()
         .unwrap();
