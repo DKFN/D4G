2665ae58e63af2f45eaa93f600fe3e0d71ffdc16
Alan Thorigny <alan.thorigny@epita.fr>
2019-11-20 15:14:46 +0100

    Update kick instance, remove data persitence for development purpose

--- prod_kickinstance.sh
+++ prod_kickinstance.sh
@@ -10,7 +10,8 @@ docker stop backend || true
 docker rm -f backend || true
 docker rm -f postgres || true
 docker run --name backend -d -p 80:8080 dkfn/d4g:dev
-docker run --name postgres -e POSTGRES_USER="d4g" -e POSTGRES_PASSWORD="Design4Green" -v /var/postgres/data:/var/lib/postgresql/data -p 5432:5432 -d postgres
+#docker run --name postgres -e POSTGRES_USER="d4g" -e POSTGRES_PASSWORD="Design4Green" -v /var/postgres/data:/var/lib/postgresql/data -p 5432:5432 -d postgres
+docker run --name postgres -e POSTGRES_USER="d4g" -e POSTGRES_PASSWORD="Design4Green" -p 5432:5432 -d postgres
 sleep 1
 docker exec -i postgres psql -U d4g -d d4g < ./init.sql
 
