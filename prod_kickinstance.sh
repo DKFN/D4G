c381d5b6e60aacbeb427edd24229b626531fb221
Alan Thorigny <alan.thorigny@epita.fr>
2019-11-20 15:08:10 +0100

    Update kick instance for deleting the postgresql container at the beginning and debug postgresql container init

--- prod_kickinstance.sh
+++ prod_kickinstance.sh
@@ -8,8 +8,9 @@ docker pull dkfn/d4g:dev
 docker logs backend > old_run.txt || true
 docker stop backend || true
 docker rm -f backend || true
+docker rm -f postgres || true
 docker run --name backend -d -p 80:8080 dkfn/d4g:dev
 docker run --name postgres -e POSTGRES_USER="d4g" -e POSTGRES_PASSWORD="Design4Green" -v /var/postgres/data:/var/lib/postgresql/data -p 5432:5432 -d postgres
 sleep 1
-docker exec -i postgres psql -U d4g -d d4g < init.sql
+docker exec -i postgres psql -U d4g -d d4g < ./init.sql
 
