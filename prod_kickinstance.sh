1872a2dfe77a7bf928b7e51dbadf60a15670edb2
Alan Thorigny <alan.thorigny@epita.fr>
2019-11-20 14:56:46 +0100

    Update the sequence of kick instance by adding the creation of postgres container and initialization

--- prod_kickinstance.sh
+++ prod_kickinstance.sh
@@ -9,4 +9,7 @@ docker logs backend > old_run.txt || true
 docker stop backend || true
 docker rm -f backend || true
 docker run --name backend -d -p 80:8080 dkfn/d4g:dev
+docker run --name postgres -e POSTGRES_USER="d4g" -e POSTGRES_PASSWORD="Design4Green" -v /var/postgres/data:/var/lib/postgresql/data -p 5432:5432 -d postgres
+sleep 1
+docker exec -i postgres psql -U d4g -d d4g < init.sql
 
