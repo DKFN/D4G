a84d793f1f8bcac093fa834aa0794cb6c0fc0daf
Alan Thorigny <alan.thorigny@epita.fr>
2019-11-21 11:56:07 +0100

    Bind the data of releve to the table

--- frontend/src/js/dashboard.ts
+++ frontend/src/js/dashboard.ts
@@ -8,9 +8,22 @@ function onDashboard(data) {
     dashboard.querySelector(['[bind="locataire"]']).innerHTML = data.locataire.prenom + ' ' + data.locataire.nom;
     dashboard.querySelector(['[bind="pieces"]']).innerHTML = data.nb_pieces + ' pièce' + (data.nb_pieces != '1' ? 's' : '') || 'Non renseignée';
     dashboard.querySelector(['[bind="ville"]']).innerHTML = data.ville || 'Non renseignée';
+    onArray(data);
 
     console.log('ON DASHBOARD : ', data);
 
+}
+
+function onArray(data) {
+    const table: HTMLTableElement = <HTMLTableElement> $.id('table-releve-user');
+    const reversed = data.releves.reverse();
+    reversed.forEach(element => {
+        let row: HTMLTableRowElement = <HTMLTableRowElement> table.insertRow();
+        let cellDate: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
+        let cellValeur: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
+        cellDate.innerHTML = element.date;
+        cellValeur.innerHTML = element.valeur;
+    });
 }
 
 export { onDashboard };
