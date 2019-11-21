665fb2a22f1a85a9843dac39a1e1c40f6fb97ee0
Fabio.ROCCO <fabio.rocco@green-conseil.com>
2019-11-21 02:10:59 +0100

    Dashboard: First version of the dashboard for a foyer

--- frontend/src/js/dashboard.ts
+++ frontend/src/js/dashboard.ts
@@ -1,7 +1,16 @@
+import { clean } from "./dom";
 import { $ } from "./dollard";
 
 function onDashboard(data) {
+    clean('page-dashboard-user');
+    const dashboard = $.id('page-dashboard-user');
+    dashboard.querySelector(['[bind="proprietaire"]']).innerHTML = data.proprietaire.societe || data.proprietaire.prenom + ' ' + data.proprietaire.nom;
+    dashboard.querySelector(['[bind="locataire"]']).innerHTML = data.locataire.prenom + ' ' + data.locataire.nom;
+    dashboard.querySelector(['[bind="pieces"]']).innerHTML = data.nb_pieces + ' pièce' + (data.nb_pieces != '1' ? 's' : '') || 'Non renseignée';
+    dashboard.querySelector(['[bind="ville"]']).innerHTML = data.ville || 'Non renseignée';
+
     console.log('ON DASHBOARD : ', data);
+
 }
 
 export { onDashboard };
