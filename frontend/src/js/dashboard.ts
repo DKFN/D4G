963e5fb60920b05b672b113e6b0a187887d3b513
Fabio.ROCCO <fabio.rocco@green-conseil.com>
2019-11-21 18:19:20 +0100

    Details: Set the first elements to add a new 'releve' to a foyer

--- frontend/src/js/dashboard.ts
+++ frontend/src/js/dashboard.ts
@@ -1,18 +1,21 @@
 import { clean, bind } from "./dom";
+import { onDetails } from "./details";
 import { $ } from "./dollard";
 
 function onDashboard(data, admin) {
     clean('page-dashboard-user');
     const dashboard = $.id('page-dashboard-user');
-
-    console.log(admin);
-
     bind(dashboard, {
         'proprietaire': data.proprietaire.societe || data.proprietaire.prenom + ' ' + data.proprietaire.nom,
         'locataire': data.locataire.prenom + ' ' + data.locataire.nom,
         'pieces': data.nb_pieces + ' pièce' + (data.nb_pieces != '1' ? 's' : '') || 'Non renseignée',
         'ville': data.ville || 'Non renseignée'
     });
+
+    dashboard.querySelector('[action="open-modal"]').onclick = () => {
+        onDetails(dashboard, data.foyer)
+    };
+
     onArray(data);
 }
 
