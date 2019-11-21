ddd78d7255136042c4954dcd58f0f13f3bb57256
Alan Thorigny <alan.thorigny@epita.fr>
2019-11-21 18:39:09 +0100

    Delete onArrayAdmin from dashboard file

--- frontend/src/js/dashboard.ts
+++ frontend/src/js/dashboard.ts
@@ -38,25 +38,4 @@ function onArrayUser(tableId, data) {
     });
 }
 
-
-function onArrayAdmin(tableId, data) {
-    const table: HTMLTableElement = <HTMLTableElement> $.id(tableId);
-
-    data.forEach(function (item) {
-       let row: HTMLTableRowElement = <HTMLTableRowElement> table.insertRow();
-       let cellFoyer: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
-       let cellType: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
-       let cellVille: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
-       let cellLocataire: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
-       let cellProprietaire: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
-       let cellButton: HTMLTableCellElement = <HTMLTableCellElement> row.insertCell();
-       cellFoyer.innerHTML = item.foyer;
-       cellType.innerHTML = item.l_type;
-       cellVille.innerHTML = item.ville;
-       cellLocataire.innerHTML = item.locataire_prenom + " " + item.locataire_nom;
-       cellProprietaire.innerHTML = item.proprietaire_societe ? item.proprietaire_societe : item.proprietaire_prenom + " " + item.proprietaire_nom;
-       cellButton.innerHTML = '<a href="#">Plus</a>';
-    });
-}
-
 export { onDashboard, onDashboardAdmin };
