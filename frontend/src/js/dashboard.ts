a4b1b9fa9f8ce365dcea3a0d3b70ac5a850912dc
Fabio.ROCCO <fabio.rocco@green-conseil.com>
2019-11-21 11:34:22 +0100

    Dom: Create a method that simplify the injection of data into the dom

--- frontend/src/js/dashboard.ts
+++ frontend/src/js/dashboard.ts
@@ -1,16 +1,15 @@
-import { clean } from "./dom";
+import { clean, bind } from "./dom";
 import { $ } from "./dollard";
 
 function onDashboard(data) {
     clean('page-dashboard-user');
     const dashboard = $.id('page-dashboard-user');
-    dashboard.querySelector(['[bind="proprietaire"]']).innerHTML = data.proprietaire.societe || data.proprietaire.prenom + ' ' + data.proprietaire.nom;
-    dashboard.querySelector(['[bind="locataire"]']).innerHTML = data.locataire.prenom + ' ' + data.locataire.nom;
-    dashboard.querySelector(['[bind="pieces"]']).innerHTML = data.nb_pieces + ' pièce' + (data.nb_pieces != '1' ? 's' : '') || 'Non renseignée';
-    dashboard.querySelector(['[bind="ville"]']).innerHTML = data.ville || 'Non renseignée';
-
-    console.log('ON DASHBOARD : ', data);
-
+    bind(dashboard, {
+        'proprietaire': data.proprietaire.societe || data.proprietaire.prenom + ' ' + data.proprietaire.nom,
+        'locataire': data.locataire.prenom + ' ' + data.locataire.nom,
+        'pieces': data.nb_pieces + ' pièce' + (data.nb_pieces != '1' ? 's' : '') || 'Non renseignée',
+        'ville': data.ville || 'Non renseignée'
+    });
 }
 
 export { onDashboard };
